5e28354592895dc5e35004be7f4eb1854af5462b
Kripa Nand Kumar Sah <engineerk.k.s6@gmail.com>
2021-02-14 23:25:11 +0530

    Day2 Compeleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:awesom/Home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,13 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to my New app"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
