8b5a0ee7e0cad688088f7bd8a5b4d886a0050594
engineerkks6 <engineerk.k.s6@gmail.com>
2021-02-22 12:41:23 +0530

    Day 12 Compeleted

--- lib/Pages/Home_page.dart
+++ lib/Pages/Home_page.dart
@@ -1,22 +1,33 @@
+import 'package:awesom/models/catalog.dart';
 import 'package:awesom/widgets/drawer.dart';
+import 'package:awesom/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Ayush Kumar";
+
+  final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to my $days $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          // itemCount: CatalogModel.items.length,
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              // item: CatalogModel.items[index],
+              item: dummyList[index],
+            );
+          },
         ),
       ),
-      drawer: MyDrawer(
-      ),
+      drawer: MyDrawer(),
     );
   }
 }
